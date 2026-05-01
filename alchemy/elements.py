6e9f60d5fc3abea29ed749ed7486e5aadd259141
David Garcia Solis <davgarc4@c3r5s2.42madrid.com>
2026-05-01 13:16:47 +0200

    primer ejercicio

---
+++ alchemy/elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def create_fire() -> None:
+	return ("Fire element created")
+
+def create_water() -> None:
+	return ("Water element created")
+
+def create_earth() -> None:
+	return ("Earth element created")
+
+def create_air() -> None:
+	return ("Air element created")
